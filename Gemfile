b2b50b34df0f46a02d837208b14d5a65f0f5c7da
Alisa Chang <laptite@gmail.com>
2020-03-15 16:21:12 -0700

    Add masonry, jquery and update Pics index and show styling

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'simple_form', '~> 5.0', '>= 5.0.2'
 gem 'paperclip', '~> 6.1'
 gem 'bootstrap-sass', '~> 3.4', '>= 3.4.1'
 gem 'jquery-rails'
+gem 'masonry-rails', '~> 0.2.4'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/pic.rb
+++ app/models/pic.rb
@@ -2,6 +2,6 @@ class Pic < ApplicationRecord
 	belongs_to :user
 	validates :title, :description, presence: true
 
-	has_attached_file :image, styles: { medium: "300x300>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :image, styles: { medium: "350>x350" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
